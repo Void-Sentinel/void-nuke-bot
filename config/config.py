c99c38b3068546678e067641888e3c15f71ee473
Mr Stonkles <voby@waifu.club>
2026-08-19 18:12:40 +0600

    rewrite

---
+++ config/config.py
@@ -0,0 +1,8 @@
+from .token import TOKEN
+
+headers = {"Authorization": f"Bot {TOKEN}"}
+LINKSERV = "https://github.com/Ramimnur20 | https://t.me/vroffteam"
+CHNAME = "Voidteam"
+ROLENAME = "Void"
+TEXT = "Void. https://github.com/Ramimnur20"
+EVENT_NAME = "Void"
