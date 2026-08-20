ba364c8c1841104831bd7bc86bf2ed819ada623e
Mr Stonkles <voby@waifu.club>
2026-08-20 17:11:23 +0600

    Add main.py (bot.py)

---
+++ main.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+import disnake
+from art import text2art
+from disnake.ext import commands
+
+from core.config.token import TOKEN
+
+logger = logging.getLogger(__name__)
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("V.", "v.", "!", "."),
+            intents=disnake.Intents.all(),
+        )
+
+    async def on_ready(self):
+        print(text2art("Void", font="fire_font-s"))
+        print("""
+        [#] Started Void NB
+        [#] Developer: voby7 | github.com/Ramimnur20
+              """)
+        logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
+        self.load_cogs()
+        self.print_invite_link()
+
+    def load_cogs(self):
+        for filename in os.listdir("modules"):
+            if filename.endswith(".py") and not filename.startswith("__"):
+                self.load_extension(f"modules.{filename[:-3]}")
+                logger.info(f"Loaded {filename[:-3]} loaded!")
+
+    def print_invite_link(self):
+        permissions = disnake.Permissions(permissions=8)
+        invite_link = disnake.utils.oauth_url(self.user.id, permissions=permissions)
+        logger.info(f"Invite link for the bot: {invite_link}")
+
+
+def main():
+    Bot().run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
