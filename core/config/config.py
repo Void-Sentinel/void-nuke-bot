31198525389b8f9108067ef0b4e36a66713b0267
Mr Stonkles <voby@waifu.club>
2026-08-20 18:55:55 +0600

    restore original channel names.

--- core/config/config.py
+++ core/config/config.py
@@ -1,9 +1,17 @@
 from core.config.token import TOKEN
 # create file token.py with constant TOKEN
 # example: TOKEN = "token"
+NAME = "Void"
 
 headers = {"Authorization": f"Bot {TOKEN}"}
 LINKSERV = "https://discord.gg/Y6qZ4TKRM5"
-CHNAME = ["Voidteam", "Void", "Nuked", "Rip", "Deleted", "Gone", "Wiped", "Owned"]
+CHNAME = [
+    f"「🖤」raped by {NAME}",
+    f"「😂」nuked by {NAME}",
+    f"「📢」fucked by {NAME}",
+    f"「👹」obliterated by {NAME}",
+    f"「💀」clowned by {NAME}",
+    f"「💣」swirled by {NAME}",
+    f"「👺」toxxed by {NAME}",
+    ]
 TEXT = "Void. https://github.com/Ramimnur20"
-NAME = "Void"
