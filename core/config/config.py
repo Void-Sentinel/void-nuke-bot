4564773d562df4e1043975aca41e2740a3ab76c2
Mr Stonkles <voby@waifu.club>
2026-08-20 17:09:21 +0600

    Add core/config (token + config)

---
+++ core/config/config.py
@@ -0,0 +1,11 @@
+from core.config.token import TOKEN
+
+# create file token.py with constant TOKEN
+# example: TOKEN = "token"
+
+headers = {"Authorization": f"Bot {TOKEN}"}
+LINKSERV = "https://github.com/Ramimnur20 | https://t.me/vroffteam"
+CHNAME = "Voidteam"
+ROLENAME = "Void"
+TEXT = "Void. https://github.com/Ramimnur20"
+EVENT_NAME = "Void"
